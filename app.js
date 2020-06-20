f7bb2f0bb8c5635b3577e504fd8da7a1c625899d
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-20 09:55:13 -0300

    first component

---
+++ app.js
@@ -0,0 +1,14 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui stackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
