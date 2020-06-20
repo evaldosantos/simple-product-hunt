53a6d2b2348718692abcbe4d6c51aa29aa57d4b3
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-20 11:10:05 -0300

    making product data-driven

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 class ProductList extends React.Component {
   render() {
+    const [product] = Seed.products;
+
     return (
       <div className='ui stackable items'>
-        <Product />
+        <Product 
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl} />
       </div>
     )
   }
@@ -10,19 +19,39 @@ class ProductList extends React.Component {
 
 class Product extends React.Component {
   render() {
+    const {
+      id,
+      title,
+      description,
+      url,
+      votes,
+      submitterAvatarUrl,
+      productImageUrl,
+    } = this.props;
+
     return (
       <div className="item">
         <div className="image">
-          <img src="images/products/image-aqua.png" />
+          <img src={productImageUrl} />
         </div>
         <div className="middle aligned content">
+          <div className='header'>
+            <a>
+              <i className='large caret up icon' />
+            </a>
+            {votes}
+          </div>
           <div className="description">
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two week</p>
+            <a href={url}>
+              {title}
+            </a>
+            <p>
+              {description}
+            </p>
           </div>
           <div className="extra">
             <span>Submitted by:</span>
-            <img className="ui avatar image" src="images/avatars/daniel.jpg" />
+            <img className="ui avatar image" src={submitterAvatarUrl} />
           </div>
         </div>
       </div>
